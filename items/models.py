71a2b61e15597279dac9d3a23bcd50cc936c8247
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-09-16 01:57:50 +0530

    Create new model and migrations

--- items/models.py
+++ items/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Category(models.Model):
@@ -10,3 +11,17 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    price = models.FloatField()
+    is_sold = models.BooleanField(default=False)
+    images = models.ImageField(upload_to="images", blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, related_name="items" ,on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, related_name="items", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} By {self.created_by}"

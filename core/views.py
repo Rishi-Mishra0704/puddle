e519b1921dd6af197832885fe2f9b23a1ce29577
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-09-16 20:20:07 +0530

    Add signup page

--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from items.models import Category, Item
 from .forms import SignUpForm
 # Create your views here.
@@ -16,3 +16,21 @@ def index(request):
 
 def contact(request):
     return render(request, "core/contact.html")
+
+
+def signup(request):
+    form = SignUpForm()
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            print("valid")
+            form.save()
+            return redirect("/login/")
+        else:
+            form = SignUpForm()
+            print("invalid")
+            print(form.errors)
+    context = {
+        "form": form
+    }
+    return render(request, "core/signup.html", context)

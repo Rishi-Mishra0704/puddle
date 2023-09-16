c086f968f12ac6e373fb80aceface900cd7f0cd8
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-09-16 12:29:18 +0530

    Complete Homepage

--- core/views.py
+++ core/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-
+from items.models import Category,Item
 # Create your views here.
 def index(request):
-    return render(request, "core/index.html")
+    items = Item.objects.filter(is_sold=False).order_by('-created_at')[:6]
+    categories = Category.objects.all()
+    context = {
+        "items": items,
+        "categories": categories
+    }
+    return render(request, "core/index.html", context)
 
 def contact(request):
     return render(request, "core/contact.html")
